38f0e72d530281faf5790502bae8f0de5727da9d
Raymond Wolt <rjwolt@gmail.com>
2021-03-28 10:22:34 -0500

    Working autocomplete search  with display controller

--- src/app.js
+++ src/app.js
@@ -1,24 +1,102 @@
 import './style.css';
 import axios from 'axios';
+import { Loader } from '@googlemaps/js-api-loader';
 
-const BASE_URL = 'api.openweathermap.org/data/2.5/weather?zip=';
+// async function getBrowserLocation() {
+//   navigator.geolocation.getCurrentPosition(success, error);
+// }
+
+// async function success(position) {
+//   let browserWeather = await getWeather([
+//     position.coords.latitude,
+//     position.coords.longitude,
+//   ]);
+//   console.log(browserWeather);
+// }
+
+// function error(err) {
+//   console.error(`ERROR: ${err.code}: ${err.message}`);
+// }
+
+// getBrowserLocation();
+
+//Parameters for OpenWeather API
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall';
 const API_KEY = '96fbe31792d3ab0a6f8838c62dde7ca5';
 
-async function getWeather(zip) {
-  let response = await axios.get(`http://${BASE_URL}${zip}&appid=${API_KEY}`);
+//Load Google Maps API
+const loader = new Loader({
+  apiKey: 'AIzaSyBrlrNOQ1WhttjwHkXEuQgUf-OpogzGKig',
+  version: 'weekly',
+  libraries: ['places'],
+});
+
+//Start the Autocomplete service
+let autocomplete;
+loader.load().then(() => {
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById('autocomplete'),
+    {
+      types: ['(cities)'],
+      fields: ['geometry', 'adr_address'],
+    }
+  );
+  autocomplete.addListener('place_changed', onPlaceChanged);
+});
+
+//Callback when place is selected
+function onPlaceChanged() {
+  let place = autocomplete.getPlace();
+
+  if (!place.geometry) {
+    //User did not select a prediction; reset the input field
+    document.getElementById('autocomplete').placeholder = 'Search Locations';
+  } else {
+    //Assign place details to variables and call display controller
+    let city = {};
+    city.name = place.adr_address;
+    city.lat = place.geometry.location.lat();
+    city.lon = place.geometry.location.lng();
+    viewer
+      .displayWeather(city)
+      .then((weather) => viewer.updateForecast(weather));
+  }
+}
+
+async function getWeather([lat, lon]) {
+  let response = await axios.get(
+    `${BASE_URL}?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${API_KEY}`
+  );
   return response.data;
 }
 
-async function main() {
-  let currentWeather = await getWeather(78520);
-  console.log(currentWeather);
-  document.querySelector('.city-name').innerText = currentWeather.name;
-  document.querySelector('.current-temp').innerText = convertF(
-    currentWeather.main.temp
-  );
-}
-
-main();
+const viewer = (() => {
+  let weather;
+  //Display the current weather
+  const displayWeather = async (city) => {
+    weather = await getWeather([city.lat, city.lon]);
+    document.querySelector('#city-name').innerHTML = city.name;
+    document.querySelector('#current-temp').innerText = convertF(
+      weather.current.temp
+    );
+  };
+  //Display the 7 day forecast
+  const updateForecast = () => {
+    let forecast = document.querySelector('#forecast');
+    forecast.innerHTML = '';
+    weather.daily.forEach((day) => {
+      let card = document.createElement('div');
+      card.classList.add('card');
+      let info = document.createElement('p');
+      info.innerHTML += new Date(day.dt * 1000).toString() + '</br>';
+      info.innerHTML += 'Low: ' + convertF(day.temp.min) + '</br>';
+      info.innerHTML += 'High: ' + convertF(day.temp.max) + '</br>';
+      card.appendChild(info);
+      forecast.appendChild(card);
+    });
+  };
+  return { displayWeather, updateForecast };
+})();
 
 function convertF(degreesKelvin) {
   return (((degreesKelvin - 273.15) * 9) / 5 + 32).toFixed(0);
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/i,
+        use: ['source-map-loader'],
+      },
     ],
   },
   devServer: {
